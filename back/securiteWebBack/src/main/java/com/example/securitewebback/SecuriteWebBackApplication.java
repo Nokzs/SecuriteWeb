15b31c8fc7ce03fe14bd6e4a66015e9d6745d95a
perez112u <coganperez@gmail.com>
2026-01-14 10:20:20 +0100

    Initial commit - Spring Boot project

---
+++ back/securiteWebBack/src/main/java/com/example/securitewebback/SecuriteWebBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitewebback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuriteWebBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuriteWebBackApplication.class, args);
+    }
+
+}
